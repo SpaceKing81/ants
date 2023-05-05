a33e860a6cc00192d652fa54311f0f3d298a551d
SpaceKing41 <98856648+SpaceKing41@users.noreply.github.com>
2023-05-05 18:11:55 -0400

    start point

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, test");
+use macroquad::prelude::*;
+
+mod ants;
+mod ants_collection;
+
+use ants_collection::BiotCollection;
+
+fn window_conf() -> Conf {
+    Conf {
+        window_title: "Life".to_owned(),
+        fullscreen: true,
+        ..Default::default()
+    }
+}
+
+#[macroquad::main(window_conf())]
+async fn main() {
+
+    rand::srand(miniquad::date::now().to_bits());
+    let mut biots = BiotCollection::new(600);
+
+    loop {
+        biots.step();
+        clear_background(Color::new(0.,0.,0.1,1.0));
+        biots.draw();
+        draw_text(&format!("FPS: {}, biots: {}", get_fps(), biots.len()),
+            screen_width()-200., screen_height()-5.,
+            18.,
+            LIGHTGRAY);
+        next_frame().await
+    }
 }
