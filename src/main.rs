f5cd104a4d9fd633ca770b8f065d3b038b93789d
Mohammed Vohra <mohammedvohra5@gmail.com>
2025-01-25 19:01:40 -0500

    cleaned up for round 4

--- src/main.rs
+++ src/main.rs
@@ -26,31 +26,19 @@ async fn main() {
   //     colonies[i] = holder::Collection::new_collection(200)
   // }
   // let mut mappy = Collection::new_collection(500);
-  let test1 = Ant::initial_spawn(
-    750, 500, 0
-  );
-  let mut test0: Vec<Ant> = vec![test1.0,test1.1,test1.2,test1.3,test1.4,test1.5,test1.6];
-  for _i in 0..2 {
-    let test1 = Ant::initial_spawn(
-      750, 500, 0
-    );
-    let mut temp2: Vec<Ant> = vec![test1.0,test1.1,test1.2,test1.3,test1.4,test1.5,test1.6];
-    test0.append(&mut temp2);
-  }
-  for i in 0..((&test0).len()) {test0[i].accelerate(1);}
-
   loop {
     //quit option
     clear_background(Color::new(0.,0.,0.1,1.0));
     if is_key_down(KeyCode::Escape){
       break;
     }
-    if is_mouse_button_down(MouseButton::Left) {
 
+    if is_mouse_button_down(MouseButton::Left) {
+      //No clue whats in here
     }
-    // for mut i in 0..colonies.len() {
-    // // Draw the simulation
-      
+    
+
+    // Draw the simulation
       // /*
     //   Testing
     {
@@ -58,46 +46,22 @@ async fn main() {
       {
 
       }
-      // Random move Ants
-      {
-        for i in 0..((&test0).len()) {
-          test0[i].move_forward();
-          let chance = rand::gen_range(0, 10);
-          if chance > 5 {
-            match chance {
-              6=> test0[i].turn_far_left(),
-              7=> test0[i].turn_left(),
-              8=> test0[i].turn_right(),
-              9=> test0[i].turn_far_right(),
-              _=> panic!("Test error: Movement")
-            }
-          }
-        }
-      }
       
 
       //Draw
       {
-        let temp = test0.clone();
-        let it = temp.iter();
-        for i in it {
-          i.draw_ant();
-        }
+        
+
+
         draw_text(&format!("time: {}",get_time().round()), screen_width()-500., screen_height()-5., 18., LIGHTGRAY);
       }
     }
         // */
 
-      // // ColonyImplimintation::draw_colony(&colony);
-      // // draw_text(&format!("FPS: {}, ants: {}, Food: {}, SD: {}x{}, time: {}", get_fps(), ants.len(), pieces.len(), screen_width(), screen_height(), get_time()),
-      // // screen_width()-500., screen_height()-5.,
-      // // 18.,
-      // // LIGHTGRAY);
-      //     holder::Collection::step(&mut colonies[i]);
-      // }
-      // Collection::test(&mappy);
-
-      // Collection::step(&mut mappy);
+      // draw_text(&format!("FPS: {}, ants: {}, Food: {}, SD: {}x{}, time: {}", 
+      // get_fps(), ants.len(), pieces.len(), screen_width(), screen_height(), get_time()),
+      // screen_width()-500., screen_height()-5., 18., LIGHTGRAY);
+      
       next_frame().await
   }
 }
