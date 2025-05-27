97e48c0958f6c7f087882f305eda2457eb3c3912
Mohammed Vohra <mohammedvohra5@gmail.com>
2025-05-27 17:49:31 -0400

    planning plus basic macroquad setup

--- src/main.rs
+++ src/main.rs
@@ -3,3 +3,14 @@ mod matrix;
 fn main() {
 
 }
+
+/*
+
+Plan:
+  - Ant trait, implemented by the 5 different ant types: Queen, Soldier, Worker, Explorer, Defender
+  - A Food type, contains {
+    pos
+    mass - amount
+  }
+  - Don't need a colony? We will see how this goes
+*/
