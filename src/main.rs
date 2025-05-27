5bb992a17194b13a612c7717ebfddd46f159c069
Mohammed Vohra <mohammedvohra5@gmail.com>
2025-05-27 17:50:35 -0400

    forgot the macroquad part

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,21 @@
 mod matrix;
+use macroquad::prelude::*;
 
-fn main() {
 
+fn window_conf() -> Conf {
+
+  // Personal screen size for best results
+  // For rn at least
+  let pdx = 1512;
+  let pdy = 1964;
+  
+  Conf {
+      window_title: "Ants".to_owned(),
+      fullscreen: true,
+      window_height:pdy,
+      window_width:pdx,
+      ..Default::default()
+    }
 }
 
 /*
@@ -14,3 +28,47 @@ Plan:
   }
   - Don't need a colony? We will see how this goes
 */
+
+#[macroquad::main(window_conf)]
+async fn main() {
+
+  loop {
+    //quit option
+    clear_background(Color::new(0.,0.,0.1,1.0));
+    if is_key_down(KeyCode::Escape){
+      break;
+    }
+
+    if is_mouse_button_down(MouseButton::Left) {
+      //No clue whats in here
+    }
+    
+
+    // Draw the simulation
+      // /*
+    //   Testing
+    {
+      // Test something
+      {
+
+      }
+      
+
+      //Draw
+      {
+        
+
+
+        draw_text(&format!("time: {}",get_time().round()), screen_width()-500., screen_height()-5., 18., LIGHTGRAY);
+      }
+    }
+        // */
+
+      // draw_text(&format!("FPS: {}, ants: {}, Food: {}, SD: {}x{}, time: {}", 
+      // get_fps(), ants.len(), pieces.len(), screen_width(), screen_height(), get_time()),
+      // screen_width()-500., screen_height()-5., 18., LIGHTGRAY);
+      
+      next_frame().await
+  }
+}
+
